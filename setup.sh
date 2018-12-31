e50771756815769322fb6dcfba85761ba511576a
Daniel Linder <dan@linder.org>
2018-12-31 09:11:39 -0600

    Minimally working setup code.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,63 @@
 #!/bin/bash
+set -e  # Stop on any error
+set -u  # Stop when using any undefined variable
+
+# NOTES:
+# If the docer system gets confused, you can clean out all running containers
+# sudo docker stop $(docker ps -a -q) && \
+# sudo docker update --restart=no $(docker ps -a -q) && \
+# sudo systemctl restart docker
 
 # 1: Setup ssh keys if needed
 for X in git jenkins tower ; do
   cd ${X}
-  ssh-keygen -f ${X}_key
+  if [ ! -e "${X}_key" ] ; then
+    ssh-keygen -N '' -f ${X}_key
+  else
+    echo "Key for ${X} exists: $(ls -1 ${X}_key)"
+  fi
   cd ..
 done
+
+# Pull down pre-built files from other repos.
+# # 1. Artifactory and Xray
+# if [ ! -e artifactory-docker-examples ] ; then
+#   git clone https://github.com/jfrog/artifactory-docker-examples.git
+# fi
+# cd artifactory-docker-examples
+# git pull
+# cd ..
+# if [ ! -e artifactory ] ; then
+#   ln -s artifactory-docker-examples/docker-compose/artifactory .
+# fi
+
+# Setup persistent directories
+mkdir -p ${PWD}/data
+sudo ln -fs ${PWD}/data /data
+# # Artifactory
+# mkdir -p ${PWD}/data/postgresql
+# mkdir -p ${PWD}/data/artifactory
+# Git
+mkdir -p ${PWD}/data/git
+# Jenkins
+mkdir -p ${PWD}/jenkins_home
+# Ansible
+mkdir -p ${PWD}/data/ansible
+
+# Stop any previously running containers
+sudo docker stop $(docker ps -a -q) >/dev/null 2>&1
+
+# Start up systems
+# Tower, Git, Jenkins
+docker-compose build
+docker-compose up -d
+
+# # Artifactory and XRay
+# docker-compose -f artifactory/artifactory-oss-postgresql.yml up -d
+
+# Finished!  Should see all our running images:
+echo "FINISHED - Should see all the running images"
+docker ps
+
+echo "Now pull up a browser to the Jenkins system and enter in"
+echo "this one-time password: $(docker exec -ti jenkins1 cat /var/jenkins_home/secrets/initialAdminPassword)"
